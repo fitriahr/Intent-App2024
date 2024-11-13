af0d3c7196f4617e1aa0285209ff792caca998c4
fitriahr <fitriah24310006@stimata.ac.id>
2024-11-14 03:03:52 +0900

    memulai aplikasi intent
    memulai aplikasi intent

---
+++ app/src/main/java/com/example/intentapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.intentapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnMoveActivityy = findViewById<Button>(R.id.btn_move_activity)
+        btnMoveActivityy.setOnClickListener {
+            OnClick()
+        }
+    }
+    private fun OnClick(){
+        val intent = Intent(applicationContext, MoveActivityy::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/intentapp/MoveActivityy.kt
@@ -0,0 +1,31 @@
+package com.example.intentapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MoveActivityy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_move_activityy)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val btnBackActivity = findViewById<Button>(R.id.btn_back_activity)
+        btnBackActivity.setOnClickListener {
+            OnClick()
+        }
+    }
+
+    private fun OnClick() {
+        val intent = Intent(applicationContext, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
